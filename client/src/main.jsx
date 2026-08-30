98f92acf3d0f7b2fa2f355e9da32d98c7a99ecb1
yael <yaelpinhas2002@gmail.com>
2026-08-30 14:50:06 +0300

    Fix image upload flow and restore toast notifications

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,6 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <CartProvider>
         <WishlistProvider>
           <App />
+          <Toaster />
         </WishlistProvider>
       </CartProvider>
     </UserProvider>
